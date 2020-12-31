2c3f4bd49b0139e4dc125fc82caec8c01eeb5389
Emmanuel Hernandez <emmanuel.hernandez@globant.com>
2020-12-31 12:41:27 -0600

    Now scroll is disabled when the cart is opened.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { closeCart } from '../actions';
 import './Cart.scss';
 import CartProduct from './CartProduct';
 
-class Cart extends Component {
-  componentDidMount() {
-    if (this.props.cartIsOpened) {
+const Cart = ({ cart, cartIsOpened, closeCart }) => {
+  useEffect(() => {
+    if (cartIsOpened) {
       document.body.style.overflowY = 'hidden';
     }
+
+    return () => {
+      document.body.style.overflowY = 'unset';
+    };
+  }, [cartIsOpened]);
+
+  const renderedCartProducts = cart.map((product) => {
+    return <CartProduct productId={product.id} key={product.id} />;
+  });
+
+  if (!cartIsOpened) {
+    return null;
   }
 
-  componentWillUnmount() {
-    document.body.style.overflowY = 'unset';
-  }
-
-  getRenderedCartProducts = () => {
-    return this.props.cart.map((product) => {
-      return <CartProduct productId={product.id} key={product.id} />;
-    });
-  };
-
-  render() {
-    if (!this.props.cartIsOpened) {
-      return null;
-    }
-
-    return (
-      <div className="cart-bg">
-        <div className="cart">
-          <div className="close-btn" onClick={this.props.closeCart}>
-            <span className="bar first-diagonal"></span>
-            <span className="bar second-diagonal"></span>
-          </div>
-          <h2 className="has-text-weight-normal">Cart:</h2>
-          <p>
-            Subtotal: <span className="has-text-weight-bold">$315</span>
-          </p>
-          <button className="is-blue is-button">Proceed to payment</button>
-          <div>{this.getRenderedCartProducts()}</div>
-          <p>
-            Subtotal: <span className="has-text-weight-bold">$315</span>
-          </p>
-          <button className="is-blue is-button">Proceed to payment</button>
+  return (
+    <div className="cart-bg">
+      <div className="cart">
+        <div className="close-btn" onClick={closeCart}>
+          <span className="bar first-diagonal"></span>
+          <span className="bar second-diagonal"></span>
         </div>
+        <h2 className="has-text-weight-normal">Cart:</h2>
+        <p>
+          Subtotal: <span className="has-text-weight-bold">$315</span>
+        </p>
+        <button className="is-blue is-button">Proceed to payment</button>
+        <div>{renderedCartProducts}</div>
+        <p>
+          Subtotal: <span className="has-text-weight-bold">$315</span>
+        </p>
+        <button className="is-blue is-button">Proceed to payment</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return { cart: state.cart, cartIsOpened: state.cartIsOpened };
